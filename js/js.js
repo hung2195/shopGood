b9995f33eee15d079ef48b5d26edd88369f04f73
hung2195 <hung2195@gmail.com>
2018-04-25 19:46:56 +0700

    shopGoods

--- js/js.js
+++ js/js.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
 		$(this).parent().find(".sub-menu").slideToggle("400");
 		$(this).toggleClass("active");
 	});
-})
+	$('.img-details').zoom();
+	new WOW().init();
+	window.onscroll = function() {myFunction()};
+		var navbar = document.getElementById("header");
+		var sticky = navbar.offsetTop;
+		var logo = $('.logo');
+		var sticknavbar= $('.navbar');
+		var animated= $('.animated');
+		function myFunction() {
+		if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky")
+		logo.addClass('sticky-logo')
+		sticknavbar.addClass('sticky-navbar')
+		} else {
+		navbar.classList.remove("sticky");
+		logo.removeClass("sticky-logo");
+		sticknavbar.removeClass("sticky-navbar");
+		}
+		}
+});
+
